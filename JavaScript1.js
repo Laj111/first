06a76e6f002095e8261077f15d954e0abe12af92
Laj111 <104667470+Laj111@users.noreply.github.com>
2022-05-05 22:24:13 +0800

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,21 @@
+﻿function check() {
+
+	var name = document.getElementById("name").value;  //读取表单数据，创建变量
+	var pass = document.getElementById("pass").value;
+
+	if (name == "gt" && pass == "941009") {
+		alert("登录成功！");
+		window.document.f.action = "/gt.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
+	else if (name == "whl" && pass == "xxxxxxxx") {
+		alert("登录成功！");
+		window.document.f.action = "/whl.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
+
+	else {
+		alert("用户名或密码错误！");
+	}
+
+}
